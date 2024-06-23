664678cf48a162056b2853b6749c4479de618378
Sahil Thakur <163773985+sahi1thakur@users.noreply.github.com>
2024-06-23 22:27:39 +0530

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import LoginPage from './LoginPage';
+import HomePage from './HomePage';
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" render={() => <Redirect to="/login" />} />
+                <Route path="/login" component={LoginPage} />
+                <Route path="/home" component={HomePage} />
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
